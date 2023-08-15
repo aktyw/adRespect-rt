797d20902285b01b5ec0fa68d02135793177dbe3
aktyw <arek.tywonek@gmail.com>
2023-08-15 13:36:05 +0200

    feat: add navbar, add tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}", "./index.html",],
+  content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'light-gray': '#F5F0EC',
+      'light-black': '#111111',
+      green: '#1B5B31',
+      white: '#ffffff',
+      black: '#000000',
+      beige: '#DCC1AB',
+    },
+    fontFamily: {
+      text: ['Inter'],
+      headings: ['Montserrat'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
-}
-
+};
